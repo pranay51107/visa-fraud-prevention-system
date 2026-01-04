b5636e173028da5bc2594d2be0c5d424b7bc6145
pranay51107 <krishnapranayd511@gmail.com>
2026-01-05 00:28:52 +0530

    Delete services directory

--- services/searchService.js
+++
@@ -1,9 +0,0 @@
-async function verifyConsultant(name) {
-  return {
-    consultant: name || "Unknown",
-    trusted: true,
-    source: "Google Custom Search (demo)"
-  };
-}
-
-module.exports = verifyConsultant;
