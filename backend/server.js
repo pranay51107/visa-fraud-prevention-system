20e51a91419d0a71a013ec9024abcaa0e0770df6
indhirareddyv05 <indhirareddyvarikuti05@gmail.com>
2026-01-05 19:05:35 +0530

    Fix port for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,10 @@ app.get("/", (req, res) => {
 });
 
 // ✅ START SERVER
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`SERVER RUNNING ON ${PORT}`);
+  console.log(Server running on ${PORT});
 });
 
 
